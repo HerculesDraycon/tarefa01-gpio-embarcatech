0fa2fbac860808bceb8973c74cda8c7f45b84c96
Marlon Santos <82154609+marloon-mon@users.noreply.github.com>
2025-01-17 17:39:26 -0300

    feat - Criação do arquivo gpio.c e função de inicialização dos pinos dos leds

---
+++ gpio.c
@@ -0,0 +1,13 @@
+#include "gpio.h"
+
+// Inicialização dos pinos dos leds
+void led_init() {
+    gpio_init(LED_RED);
+    gpio_set_dir(LED_RED, GPIO_OUT);
+
+    gpio_init(LED_GREEN);
+    gpio_set_dir(LED_GREEN, GPIO_OUT);
+
+    gpio_init(LED_BLUE);
+    gpio_set_dir(LED_BLUE, GPIO_OUT);
+}
